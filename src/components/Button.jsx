66b3e292e60eccaeba6c5030747113a2a0e5dd3a
RamyRxr <reyr06115@gmail.com>
2025-06-29 00:19:50 +0100

    some chnages on the left side

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const Button = ({ text , className , id }) => {
-    return (
-        <a id={id} className={`${className ?? ''} cta-wrapper`}>
-            <div className="cta-button group">
-                <div className="bg-circle" />
-                    <p className="text">{ text }</p>
-                    <div className="arrow-wrapper">
-                        <img src="/images/arrow-down.svg" alt="arrow" />
-                    </div>
-                
-            </div>
-        </a>
-    )
-}
+const Button = ({ text, className, id, href }) => {
+  return (
+    <a
+      id={id}
+      href={href}
+      className={`${
+        className ?? ""
+      } cta-wrapper transition-transform duration-300 hover:scale-105`}
+    >
+      <div className="cta-button group bg-gradient-to-r from-teal-500 to-indigo-600 rounded-lg shadow-lg hover:shadow-xl">
+        <div className="bg-circle" />
+        <p className="text font-bold text-white text-lg"> {text}</p>
+        <div className="arrow-wrapper">
+          <img src="/images/arrow-down.svg" alt="arrow" />
+        </div>
+      </div>
+    </a>
+  );
+};
 
-export default Button
+export default Button;
